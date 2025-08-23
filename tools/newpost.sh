8f01310fe665ada5804bde0038d2cbf44e65c88b
Simford Dong <sim4dong@gmail.com>
2025-08-23 18:28:44 +0800

    add newpost.sh

---
+++ tools/newpost.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# create new post with datetime stamp
+
+set -x
+
+echo "---
+title: New Post
+date: $(date +%Y-%m-%d\ %H:%M:%S\ +0800)
+categories: [AI, Programming]
+tags: [Claude Code, CCR]
+---
+" > _posts/$(date +%Y-%m-%d)-new-post.md
